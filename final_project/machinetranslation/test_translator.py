3f8f33ee1f94114f2a3a08c221b436482330aaa9
lerin-ftu <lerin.fei@gmail.com>
2021-09-19 15:26:06 +0000

    Temporary changes

--- final_project/machinetranslation/test_translator.py
+++ final_project/machinetranslation/test_translator.py
@@ -1,10 +1,23 @@
 import unittest
-from translator import english_to_french
-from translator import french_to_english
+from translator import french_to_english, english_to_french
 
 class Testfrench_to_english(unittest.TestCase):
     def Test_frenchtranslation(self):
-        self.assertEqual(language_translator.translate("Bonjour"), "Hello")
+        self.assertEqual(
+            french_to_english("Bonjour"), "Hello"
+        )
+        self.assertNotEqual(
+            french_to_english("Bounjour"), "How are you"
+        )
+
+class Testenglish_to_french(unittest.TestCase):
+    def Test_englishtranslation(self):
+        self.assertEqual(
+            english_to_french("Hello"), "Bonjour"
+        )
+        self.assertNotEqual(
+            english_to_french("Hello"), "comment allez-vous"
+        )
 
 if __name__ == '__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
+"""Translator module."""
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
